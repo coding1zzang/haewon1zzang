5a5092cf0e89de022da00ce8e05b4b2e14350fcc
Lee Haewon <hawon2205@ewhain.net>
2023-02-02 21:27:09 +0900

    [입출력과 사칙연산] 230202

---
+++ week01/1001.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split()) # a, b를 공백으로 구분하여 입력
+
+# a - b 출력
+print(a - b)
---
+++ week01/1008.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split()) # a, b를 공백으로 구분하여 입력
+
+# a + b 출력
+print(a + b)
+
+# a - b 출력
+print(a - b)
+
+# a * b 출력
+print(a * b)
+
+# a / b(몫) 출력
+print(a // b) # 파이썬에서는 //로 몫을 구할 수 있음
+
+# a % b(나머지) 출력
+print(a % b)
