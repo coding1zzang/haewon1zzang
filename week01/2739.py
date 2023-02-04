d9d0cd48644d69e44d95ddce287d22a52eae43c9
Lee Haewon <hawon2205@ewhain.net>
2023-02-05 00:00:54 +0900

    [반복문] 230205

---
+++ week01/2739.py
@@ -0,0 +1,3 @@
+n = int(input()) # 구구단 n단
+for i in range(1, 10): # i는 1~9
+    print(n, '*', i, '=', n * i)
---
+++ week01/8393.py
@@ -0,0 +1,8 @@
+# 1부터 n까지 더하기
+n = int(input())
+
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+    
+print(sum)
