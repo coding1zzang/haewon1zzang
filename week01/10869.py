87cbf9878c1d1dc532a9bd2b1912db04b4a0fcda
Lee Haewon <hawon2205@ewhain.net>
2023-02-10 21:04:07 +0900

    [입출력과 사칙연산] 230210

---
+++ week01/10869.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split()) # a, b를 공백으로 구분하여 입력
+
+# a + b 출력
+print(a + b)
+
+# a - b 출력
+print(a - b)
+
+# a * b 출력
+print(a * b)
+
+# a / b 출력
+print(a // b)
+
+# a % b 출력
+print(a % b)
